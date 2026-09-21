d75adcf25928925daa8be036d717390ca4ac805c
axis <asebastian.martinf@gmail.com>
2026-09-21 16:55:19 -0500

    feature(in-app-navigation): add in-app navigation .router

---
+++ src/i18n.js
@@ -0,0 +1,10 @@
+import {createI18n} from "vue-i18n";
+import
+
+
+const i18n = createI18N (
+    {
+        "legacu
+    }
+
+)
--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import i18n from
+import router from "./router.js"; ./i18n.js
 
 createApp(App).mount('#app')
+    .use(i18n)
+    .use(router)
+    .mount("App")
